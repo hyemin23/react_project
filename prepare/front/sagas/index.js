d8a8032efdb63023606defa82eee82329c53eded
hyemin23 <hm_stom@naver.com>
2021-04-14 23:59:51 +0900

    #cors 설정 및 axios credentials 설정

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -4,6 +4,10 @@ import postSaga from "./post";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3065"
+//프론트에서는 withCredentioals : true로 보내고
+//백엔드에서는  cors에서 설정해주면 된다 
+//서로간의 쿠키가 전달됨 그래야.
+axios.defaults.withCredentials = true
 
 export default function* rootSaga() {
 
